96c13a5a363c185e0d102ced7a33f5b2a9a216cb
Thomas Hoadley <hoadleytom@gmail.com>
2020-04-22 14:31:51 +0100

    Going to change the functional component to a class component and use traditional life cycle methods with react

--- components/home/HomeIntroduction.js
+++ components/home/HomeIntroduction.js
@@ -4,77 +4,134 @@ import { GlobalContext } from '../../context/GlobalState'
 
 const HomeIntroduction = (props) => {
     const { toggleMusic } = useContext(GlobalContext)
-    let { introText, introContainer } = useRef(null)
+    let { introText, introContainer, subText } = useRef(null)
     let timeline = new TimelineLite()
 
-    useEffect(() => {
-        // set up timelines
-        let masterTimeline = timeline;
-        let introTimeLine = new TimelineLite();
+    const handleClick = () => {
+        alert('test');
+        console.log(masterTimeline);
+    }
 
-        // hide container untill all is loaded.
+    useEffect(() => {
+        // create master timeline
+
+        let masterTimeline = timeline;
+
+        // hide container untill the page is loaded
+
         TweenMax.to(introContainer, 0, { css: { visibility: 'visible' } })
 
-        // text timeline
-        const introText1 = introText.children[0]
-        const introText2 = introText.children[1]
-        const introText3 = introText.children[2]
+        // set up timeline variables
+        let introText1 = introText.children[0]
+        let introText2 = introText.children[1]
+        let introText3 = introText.children[2]
+        let subText1 = subText.children[0]
+        let subText2 = subText.children[1]
 
-        // created as a function so that it can be delayed
-        const introTimeLineFunction = () => {
-            introTimeLine
+        // create timeline functions
 
-                .staggerTo([introText1, introText2, introText3], 1.2, {
-                    y: '0%',
-                    ease: Power3.easeOut,
-                    delay: 0.8
-                }, 0.3)
+        const introTextTimeline = () => {
+            let timeline = new TimelineLite();
 
-                .staggerTo([introText3, introText2, introText1], 1.2, {
-                    y: '-100%',
-                    opacity: 0,
-                    ease: Power3.easeIn
-                }, 0.3)
-
-                .to(introContainer, 3, {
-                    backgroundColor: '#000',
-                    ease: Power3.easeOut,
-                }, ">").eventCallback('onComplete', alert)
+            timeline.staggerTo([introText1, introText2, introText3], 1.2, {
+                y: '0%',
+                ease: Power3.easeOut,
+                delay: 0
+            }, 0.3)
+            return timeline
         }
 
-        // master timeline
-        masterTimeline.add(introTimeLineFunction, "+=1");
+        const subTextAnimation = () => {
+            let timeline = new TimelineLite();
+
+            timeline.to(subText1, 1, {
+                y: '0px',
+                ease: Power3.easeOut
+            }).to(subText1, 1, {
+                y: '-18px',
+                ease: Power3.easeOut
+            }, "+=0.5").to(subText2, 1, {
+                y: '0px',
+                ease: Power3.easeOut
+            }, "-=1")
+            return timeline
+        }
+
+        // create the timeline
+
+        masterTimeline.add(introTextTimeline(), "+=1");
+        masterTimeline.add(subTextAnimation());
+        masterTimeline.add('startMusic')
 
     }, [timeline]);
 
     return (
-        <div className="introContainer" ref={el => introContainer = el}>
-            <div className="animated-text--container">
-                <h1 className="animated-text--inner" ref={el => { introText = el }}><span>Welcome</span> <span>to</span> <span>Iceland.</span></h1>
+        <div className="intro"
+            ref={el => introContainer = el}
+            onClick={() => handleClick()} >
+            <div className="intro__content">
+                <h1 className="appearing-text"
+                    ref={el => { introText = el }}>
+                    <span>Welcome</span> <span>to</span> <span>Iceland.</span>
+                </h1>
+                <p className="rolling-text"
+                    ref={el => { subText = el }}>
+                    <span>Please ensure you have sound</span>
+                    <span>Click to start your journey</span>
+                </p>
             </div>
 
             <style jsx>{`
-                .introContainer {
+                .intro {
                     height: 100vh;
                     position: relative;
                     background: #091a2f;
-
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    
                     h1 {
-                        position: absolute;
-                        top: 50%;
-                        left: 50%;
-                        color: #fff;
-                        margin-top: 0px;
-                        transform: translate(-50%,-50%);
                         font-family: 'Playfair Display', serif;
-                        font-size: calc(4em + 0.5vw);
+                        margin-top: 0px;
+                        font-size: calc(3em + 1vw);
+                        margin-bottom: 0px;
+                        opacity: 1;
+                        letter-spacing: 0.02em;
+                    }
+                    p {
+                        font-family: 'Open sans';
+                        font-weight: 300;
+                        font-size: 15px;
+                        opacity: 0.8;
+                        text-transform: uppercase;
+                        letter-spacing: 0.1em;
+                    }
+                    .appearing-text {
+                        color: #fff;
                         overflow: hidden;
-                        display: inline-block;
                         line-height: 1;
                         span {
                             position: relative;
                             display: inline-block;
                             transform: translateY(100%);
+                        }
+                    }
+                    .rolling-text {
+                        color: #fff;
+                        overflow: hidden;
+                        position: relative;
+                        line-height: 1;
+                        display: block;
+                        text-align: right;
+                        height: 18px;
+                        margin-top: 30px;
+                        text-align: center;
+                        span {
+                            position: absolute;
+                            top: 0;
+                            width: 100%;
+                            left: 50%;
+                            transform: translateY(18px) translateX(-50%);
                         }
                     }
                 }
@@ -84,3 +141,6 @@ const HomeIntroduction = (props) => {
 }
 
 export default HomeIntroduction
+
+// Helpers 
+// https://www.youtube.com/watch?time_continue=55&v=ZbTI85lmu9Q&feature=emb_logo
